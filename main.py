9b057bd1e80b2f721071444c65dd8baf174d46a1
Silas Groskreutz <silas.groskreutz@gmail.com>
2025-12-01 14:47:35 -0600

    Added verbose mode, for output counts

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-import sys  
+import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+verbose = False
+    # Check if --verbose is in the arguments
+if '--verbose' in sys.argv:
+    verbose = True
+
+if len(sys.argv) <= 1:
+    print("Please ask a question of the AI agent.")
+    sys.exit(1)
+
+user_prompt = sys.argv[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose == True:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 print(response.text)
 
