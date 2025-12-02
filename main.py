653a7f77a07cfeb94305bb825e47a5b24b9362a1
Silas Groskreutz <silas.groskreutz@gmail.com>
2025-12-02 09:01:13 -0600

    Needed to refactor main.py, as I missed grading of previous lesson

--- main.py
+++ main.py
@@ -4,35 +4,44 @@ from google import genai
 import sys
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+    
+    verbose = False
+        # Check if --verbose is in the arguments
+    if '--verbose' in sys.argv:
+        verbose = True
 
-verbose = False
-    # Check if --verbose is in the arguments
-if '--verbose' in sys.argv:
-    verbose = True
+    if len(sys.argv) <= 1:
+        print("Please ask a question of the AI agent.")
+        sys.exit(1)
 
-if len(sys.argv) <= 1:
-    print("Please ask a question of the AI agent.")
-    sys.exit(1)
+    user_prompt = sys.argv[1]
 
-user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-)
+    if verbose == True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-if verbose == True:
-    print(f"User prompt: {user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
 
-print(response.text)
+
+if __name__ == "__main__":
+    main()
 
