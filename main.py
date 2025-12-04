993367228006e579bf4f22be8bfaff240015a127
Silas Groskreutz <silas.groskreutz@gmail.com>
2025-12-04 09:18:41 -0600

    Added functions t allow AI to call functions, to iterate over them, and tested that it run and correct them

--- main.py
+++ main.py
@@ -1,47 +1,89 @@
+import argparse
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
-import sys
 from google.genai import types
+
+from call_function import available_functions, call_function
+from config import MAX_ITERS
+from prompts import system_prompt
 
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
-    
-    verbose = False
-        # Check if --verbose is in the arguments
-    if '--verbose' in sys.argv:
-        verbose = True
-
-    if len(sys.argv) <= 1:
-        print("Please ask a question of the AI agent.")
-        sys.exit(1)
-
-    user_prompt = sys.argv[1]
-
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
 
     client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, args.verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        )
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    if verbose == True:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    print(response.text)
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="user", parts=function_responses))
 
 
 if __name__ == "__main__":
     main()
-
--- test_write_file.py
+++ test_write_file.py
@@ -1,18 +1,14 @@
-from functions.write_file import write_file
+from functions.write_file_content import write_file
 
 
 def test():
     result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print("Result for current directory:")
     print(result)
-    print("")
 
     result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print("Result for 'pkg' directory:")
     print(result)
 
     result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print("Result for '/bin' directory:")
     print(result)
 
 
